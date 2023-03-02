3e0a1bca6df257f2693357392ad713a3e5608f8a
WhoAskedxD <harryvan667@gmail.com>
2023-03-02 20:01:37 +0000

    updated layouts for settings

--- src/routes/my/settings/+layout.svelte
+++ src/routes/my/settings/+layout.svelte
@@ -15,19 +15,22 @@
 		}
 	];
 </script>
-<div class="flex w-full h-full space-x-4">
-	<ul class="menu bg-base-100 w-56 p-2 rounded-box">
-		{#each navigation as navItem}
-			<li>
-				<a
-					href={navItem.href}
-					class="font-medium {$page.url.pathname === navItem.href ? 'active' : ''}"
-					>{navItem.title}</a
-				>
-			</li>
-		{/each}
-	</ul>
-	<div class="w-full">
-		<slot />
+
+<div class="flex w-full h-full flex-row justify-center">
+	<div class="w-fit flex flex-col">
+		<h1 class="text-4xl font-bold">Account</h1>
+		<p class="text-secondary">Adjust your account settings below.</p>
+		<div class="navbar bg-base-200 rounded-2xl px-1 py-1 min-h-fit mt-2">
+				<div class="navbar-center flex">
+					<ul class="menu menu-horizontal px-1 space-x-4">
+						{#each navigation as navItem}
+							<li><a href={navItem.href} class="{$page.url.pathname === navItem.href ? 'bg-base-100 text-primary font-medium' : ''} px-8 rounded-none">{navItem.title}</a></li>
+						{/each}
+					</ul>
+				</div>
+		</div>
+		<div class="w-full">
+			<slot />
+		</div>
 	</div>
 </div>
