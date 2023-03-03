3b04a0feba55a9271f2f3d076a41b89f84ec558b
WhoAskedxD <harryvan667@gmail.com>
2023-03-03 06:15:34 +0000

    made chages to the drawer toggle and profile page

--- src/routes/my/settings/profile/+page.svelte
+++ src/routes/my/settings/profile/+page.svelte
@@ -1,11 +1,11 @@
 <script>
 	import { enhance, applyAction } from '$app/forms';
 	import { invalidateAll } from '$app/navigation';
-	import { Input } from '$lib/components';
+	import { Input,Modal } from '$lib/components';
 	import { getImageURL } from '$lib/utils';
 	export let data;
 	export let form;
-	let loading;
+	$: emailModalOpen = false;
 	$: loading = false;
 	const showPreview = (event) => {
 		const target = event.target;
@@ -17,6 +17,23 @@
 			console.log(`showing preview:`,preview)
 
 		}
+	};
+	const submitUpdateEmail = () => {
+		loading = true;
+		emailModalOpen = true;
+		return async ({ result }) => {
+			switch (result.type) {
+				case 'success':
+					await invalidateAll();
+					emailModalOpen = false;
+					break;
+				case 'error':
+					break;
+				default:
+					await applyAction(result);
+			}
+			loading = false;
+		};
 	};
 	const submitUpdateProfile = () => {
 		loading = true;
@@ -121,6 +138,25 @@
 				Update Profile
 			</button>
 		</div>
-		
 	</form>
+	<div class="divider" />
+	<div class="w-full">
+		<Modal label="change-email" checked={emailModalOpen}>
+			<span slot="trigger" class="btn btn-primary w-full">Change Email</span>
+			<h3 slot="heading">Change Your Email</h3>
+			<form action="?/updateEmail" method="POST" class="space-y-2" use:enhance={submitUpdateEmail}>
+				<Input
+					id="email"
+					type="email"
+					label="Enter your new email address"
+					required={true}
+					value={form?.data?.email}
+					disabled={loading}
+				/>
+				<button type="submit" class="btn btn-primary w-full" disabled={loading}
+					>Change my email</button
+				>
+			</form>
+		</Modal>
+	</div>
 </div>
