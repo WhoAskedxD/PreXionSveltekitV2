6ac6c1d2e0c45de0abfc113688d400e8d8318f6f
WhoAskedxD <harryvan667@gmail.com>
2023-03-08 00:47:54 +0000

    learned how to use flex-1 to grow an element

--- src/lib/components/HomePage.svelte
+++ src/lib/components/HomePage.svelte
@@ -4,10 +4,10 @@
 	// console.log($page.data)
 </script>
 
-<div class="home-page h-full w-fit flex flex-col">
+<div class="home-page h-full w-full flex flex-col">
 	<div class="navbar">navbar</div>
-	<div class="card-container flex flex-row flex-1 h-[calc(80%)]">
-		<div class="task-container overflow-auto">
+	<div class="tasks-container flex flex-row flex-1 h-[calc(90%)] space-x-4">
+		<div class="card-container overflow-auto">
 			<div class="card w-96 glass">
 				<figure><img src="/images/PreXion_Sales_Territories_December_2022.jpg" alt="car!" /></figure>
 				<div class="card-body">
@@ -39,27 +39,7 @@
 				</div>
 			</div>
 		</div>
-		<div class="task-container overflow-auto">
-			<div class="card w-96 glass">
-				<figure><img src="/images/PreXion_Sales_Territories_December_2022.jpg" alt="car!" /></figure>
-				<div class="card-body">
-					<h2 class="card-title">Life hack</h2>
-					<p>How to park your car at your garage?</p>
-					<div class="card-actions justify-end">
-						<button class="btn btn-primary">Learn now!</button>
-					</div>
-				</div>
-			</div>
-			<div class="card w-96 glass">
-				<figure><img src="/images/PreXion_Sales_Territories_December_2022.jpg" alt="car!" /></figure>
-				<div class="card-body">
-					<h2 class="card-title">Life hack</h2>
-					<p>How to park your car at your garage?</p>
-					<div class="card-actions justify-end">
-						<button class="btn btn-primary">Learn now!</button>
-					</div>
-				</div>
-			</div>
+		<div class="card-container overflow-auto">
 			<div class="card w-96 glass">
 				<figure><img src="/images/PreXion_Sales_Territories_December_2022.jpg" alt="car!" /></figure>
 				<div class="card-body">
