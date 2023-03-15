b982bbeae8352b8e2e81c64a8cf63f196eae5517
WhoAskedxD <harryvan667@gmail.com>
2023-03-15 10:24:16 -0700

    fixed request to backend

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -1,10 +1,8 @@
+const backend = "https://prexionpocketbase.clearbooksaccounting.net/api/files/"
 export const serializeNonPOJOs = (obj) => {
 	return structuredClone(obj);
 };
 
 export const getImageURL = (collectionId, recordId, fileName, size = '0x0') => {
-	return `http://192.168.1.100:5000/api/files/${collectionId}/${recordId}/${fileName}?thumb=${size}`;
+	return `${backend}${collectionId}/${recordId}/${fileName}?thumb=${size}`;
 };
-export const getUserTasks = (user_id) => {
-	return `http://192.168.1.100:5000/api/collections/task/records/?filter=(user=${user_id})`;
-}
