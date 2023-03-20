ed90e582b7b3660cfa060fdf719ea852c67645b4
WhoAskedxD <harryvan667@gmail.com>
2023-03-20 09:15:15 -0700

    worked on styling for boards

--- src/lib/components/HomePage.svelte
+++ src/lib/components/HomePage.svelte
@@ -25,76 +25,8 @@
 	}
 </script>
 
-<div class="home-page h-full w-full flex flex-col">
-	<div class="tasks-container flex flex-row h-full space-x-4 overflow-scroll ml-2">
-		<!-- <div class="card-container h-fit max-h-full flex flex-col bg-slate-200 rounded-lg">
-			<div class="card-title-container flex flex-row justify-between mx-2 items-center ">
-				<div class="card-title flex-1 h-fit">
-					<input
-						type="text"
-						class="input input-ghost rounded-lg h-fit pl-2 pr-0 w-full"
-						value="value"
-					/>
-				</div>
-				<div class="flex-none">
-					<button class="btn btn-square btn-ghost">
-						<svg
-							xmlns="http://www.w3.org/2000/svg"
-							fill="none"
-							viewBox="0 0 24 24"
-							class="inline-block w-5 h-5 stroke-current"
-							><path
-								stroke-linecap="round"
-								stroke-linejoin="round"
-								stroke-width="2"
-								d="M5 12h.01M12 12h.01M19 12h.01M6 12a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0z"
-							/></svg
-						>
-					</button>
-				</div>
-			</div>
-			<div class="card-elements max-h-full overflow-y-auto overflow-x-hidden scrollbar-hide" id="card">
-				<div class="card w-96 glass">
-					<figure>
-						<img src="/images/PreXion_Sales_Territories_December_2022.jpg" alt="car!" />
-					</figure>
-					<div class="card-body">
-						<h2 class="card-title">Life hack</h2>
-						<p>How to park your car at your garage?</p>
-						<div class="card-actions justify-end">
-							<button class="btn btn-primary">Learn now!</button>
-						</div>
-					</div>
-				</div>
-				<div class="card w-96 glass">
-					<figure>
-						<img src="/images/PreXion_Sales_Territories_December_2022.jpg" alt="car!" />
-					</figure>
-					<div class="card-body">
-						<h2 class="card-title">Life hack</h2>
-						<p>How to park your car at your garage?</p>
-						<div class="card-actions justify-end">
-							<button class="btn btn-primary">Learn now!</button>
-						</div>
-					</div>
-				</div>
-				<div class="card w-96 glass">
-					<figure>
-						<img src="/images/PreXion_Sales_Territories_December_2022.jpg" alt="car!" />
-					</figure>
-					<div class="card-body">
-						<h2 class="card-title">Life hack</h2>
-						<p>How to park your car at your garage?</p>
-						<div class="card-actions justify-end">
-							<button class="btn btn-primary">Learn now!</button>
-						</div>
-					</div>
-				</div>
-			</div>
-			<div class="card-footer-container flex flex-row justify-between mx-2 my-2 items-center">
-				<button class="pl-4">+ Add Card</button>
-			</div>
-		</div> -->
+<div class="home-page h-full flex flex-col">
+	<div class="tasks-container flex flex-row h-full space-x-4 w-fit sm:flex-inital ml-2">
 		{#each boardData as board, boardIndex}
 			<div
 				class="board-container h-fit max-h-full flex flex-col rounded-md bg-primary-content w-80 py-2"
